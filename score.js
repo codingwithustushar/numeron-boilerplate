26db6d66b57a542b7eae708bacd8d5f1f1e827b4
Tushar Shekhar <tusharshekhar@Tushars-MacBook-Air.local>
2023-11-03 11:30:04 +0530

    hi

--- score.js
+++ score.js
@@ -1,1 +1,14 @@
 // Iteration 5: Store the player score and display it on the game over screen
+
+const scoreBox = document.getElementById("score-board");
+
+let passcode = localStorage.getItem("squad-56");
+console.log(passcode)
+
+scoreBox.innerHTML = passcode;
+
+const playAgainButton = document.getElementById("play-again-button")
+
+playAgainButton.onclick =()=>{
+    location.href = "./game.html"
+}
